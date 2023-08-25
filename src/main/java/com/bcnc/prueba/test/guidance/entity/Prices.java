2d902405ba66802e0f83772a2f787589813fc60c
Alejandro Rodriguez Bravo <AlejandroRodrigezBravo>
2023-08-25 20:39:42 +0200

    creacion de entidad para BBDD

--- src/main/java/com/bcnc/prueba/test/guidance/entity/Prices.java
+++ src/main/java/com/bcnc/prueba/test/guidance/entity/Prices.java
@@ -1,5 +1,124 @@
 package com.bcnc.prueba.test.guidance.entity;
 
-public class Prices {
+import java.io.Serializable;
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+/**
+ * Clase que representa a la tabla prices
+ * 
+ * @author bravo
+ * @version 24/08/2023 1.0.0
+ */
+public class Prices implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long brandId;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date startDate;
+	
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date endDate;
+
+	private Long priceList;
+
+	private Long productId;
+	
+	private String priority;
+
+	private Double price;
+
+	private String curr;
+
+	public Prices() {
+		
+	}
+	
+	public Long getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(Long brandId) {
+		this.brandId = brandId;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Long getPriceList() {
+		return priceList;
+	}
+
+	public void setPriceList(Long priceList) {
+		this.priceList = priceList;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getCurr() {
+		return curr;
+	}
+
+	public void setCurr(String curr) {
+		this.curr = curr;
+	}
+
+	@Override
+	public String toString() {
+		return "Prices [brandId=" + brandId + ", startDate=" + startDate + ", endDate=" + endDate + ", priceList="
+				+ priceList + ", productId=" + productId + ", priority=" + priority + ", price=" + price + ", curr="
+				+ curr + "]";
+	}
 
 }
